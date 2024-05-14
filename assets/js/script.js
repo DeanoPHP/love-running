a938d783a521a33e8f949c53f72e0b72cfc0fb20
Dean Lark <deanolark@gmail.com>
2024-05-14 10:25:13 +0000

    Add: added increment and incorrect score

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,8 +46,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert('Hay! You got it right! :D')
+        incrementScore()
     } else {
         alert(`awwwww..... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`)
+        incrementWrongAnswer()
     }
 
     runGame(calculatedAnswer[1])
@@ -71,12 +73,17 @@ function calculateCorrectAnswer() {
     }
 }
 
+/**
+ * Get the current score from the DOM and increment by 1
+ */
 function incrementScore() {
-
+    let oldScore = parseInt(document.getElementById('score').innerText)
+    document.getElementById('score').innerText = ++oldScore
 }
 
 function incrementWrongAnswer() {
-
+    let oldScore = parseInt(document.getElementById('incorrect').innerText)
+    document.getElementById('incorrect').innerText = ++oldScore
 }
 
 function displayAdditionQuestion(operand1, operand2) {
